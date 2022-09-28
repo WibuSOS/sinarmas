e6fe81061aa6966398ac52b81b09bf20cbcd9e1e
WibuSOS <32050596+WibuSOS@users.noreply.github.com>
2022-09-28 14:03:29 +0700

    hotfix di index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -29,11 +29,9 @@ export default function Home() {
       }
 
       <Input onSuccess={getData} />
-      {data?.data ? data.data.map((item, index) => (
+      {data?.data && data?.data?.map((item, index) => (
         <p key={index}>{item}</p>
-      )) :
-        <p>data kosong</p>
-      }
+      ))}
     </div>
   )
 }
